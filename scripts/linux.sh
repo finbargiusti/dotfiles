c3785c4f45d577c14f9dbe6ee37386565ec70d74
finbargiusti <finbargiusti@gmail.com>
2025-02-12 19:10:14 +0000

    update

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,6 +1,11 @@
-echo "Starting..."
+#!/bin/bash
 
-colima start 2> /dev/null # start if not already
+# Enters into my ubuntu container
+
+if ! colima status 2> /dev/null; then
+  echo "Starting colima"
+  colima start 2> /dev/null # start if not already
+fi
 
 docker-compose -f ~/ubuntu/docker-compose.yml up -d;
 
