fe80c1243c65381883f008c18fdcc1389058a5f1
finbargiusti <finbargiusti@gmail.com>
2025-02-25 15:20:25 +0000

    add black formatting to pyright

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -25,11 +25,26 @@ mason_lspconfig.setup {
 }
 
 mason_lspconfig.setup_handlers {
-  function(server_name)
+function(server_name)
     require('lspconfig')[server_name].setup {
       capabilities = capabilities,
       on_attach = require('lspbindings'),
       settings = servers[server_name],
+    }
+  end,
+  ['pyright'] = function() 
+    local bindings = require('lspbindings')
+    local on_attach = function(_, bufnr)
+      bindings(_, bufnr)
+      -- overwrite the lsp format command with a custom one
+      -- since pyright doesn't support formatting
+      vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.cmd('!black % > /dev/null')
+      end, { desc = 'Format current buffer with LSP', force=true })
+    end
+    require('lspconfig')['pyright'].setup {
+      capabilities = capabilities,
+      on_attach = on_attach
     }
   end,
   -- we handle this one separately
